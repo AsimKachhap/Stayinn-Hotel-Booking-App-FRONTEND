ea9616e6a1994037f9499b6694db5615b955c45c
unknown <asim26k@gmail.com>
2023-02-01 10:25:46 +0530

    Now you can map hotels fom an array

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -5,52 +5,64 @@ import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 const HotelCard = () => {
   const [isLiked, setIsLiked] = useState(false);
 
-  return (
-    <div className=" hotelCard flex flex-col  bg-[#F0F0F0] rounded-lg">
-      <div className="flex-2 w-[vw] max-h-[250px] rounded-lg">
-        <img className="rounded-t-md" src="/images/hotel1.jpg" alt="" />
-      </div>
+  const hotels = [
+    {
+      hotelName: "Shangri La",
+      city: "New Delhi",
+      address: "19, Ashoka Rd, Janpath, Connauught Place, New Delhi, Delhi",
+      PIN: 11001,
+      startingPrice: 17500,
+    },
+  ];
 
-      <div className="flex-3 grid grid-rows-2 p-2 gap-4">
-        <div className="">
-          <h2 className="font-bold text-2xl">The Phoenix Hotel</h2>
-          <div className="flex flex-row gap-2 items-center ">
-            <MdLocationOn className=" text-yellow-500 " />
-            <span className="text-gray-500 font-semibold ">
-              New Delhi, Delhi
-            </span>
-          </div>
+  return hotels.map((item) => {
+    return (
+      <div className=" hotelCard flex flex-col  bg-[#F0F0F0] rounded-lg">
+        <div className="flex-2 w-[vw] max-h-[250px] rounded-lg">
+          <img className="rounded-t-md" src="/images/hotel1.jpg" alt="" />
         </div>
 
-        <div className="flex flex-row justify-between items-center mb-4">
-          <div>
-            <p>Starts from </p>
-            <p>
-              <span className="text-2xl font-semibold">$ 250</span>
-              <span className="text-gray-500">/NIGHT</span>
-            </p>
+        <div className="flex-3 grid grid-rows-2 p-2 gap-4">
+          <div className="">
+            <h2 className="font-bold text-2xl">{item.hotelName}</h2>
+            <div className="flex flex-row gap-2 items-center ">
+              <MdLocationOn className=" text-yellow-500 " />
+              <span className="text-gray-500 font-semibold ">{item.city}</span>
+            </div>
           </div>
-          <div className="h-[30px] w-[30px] rounded-md flex items-center justify-center mr-2 ">
-            {isLiked ? (
-              <AiFillHeart
-                className="text-pink-600 h-[30px] w-[25px] "
-                onClick={() => {
-                  setIsLiked(false);
-                }}
-              />
-            ) : (
-              <AiOutlineHeart
-                className="text-gray-600 bg-transparent h-[30px] w-[25px] "
-                onClick={() => {
-                  setIsLiked(true);
-                }}
-              />
-            )}
+
+          <div className="flex flex-row justify-between items-center mb-4">
+            <div>
+              <p>Starts from </p>
+              <p>
+                <span className="text-2xl font-semibold">
+                  $ {item.startingPrice}
+                </span>
+                <span className="text-gray-500">/NIGHT</span>
+              </p>
+            </div>
+            <div className="h-[30px] w-[30px] rounded-md flex items-center justify-center mr-2 ">
+              {isLiked ? (
+                <AiFillHeart
+                  className="text-pink-600 h-[30px] w-[25px] "
+                  onClick={() => {
+                    setIsLiked(false);
+                  }}
+                />
+              ) : (
+                <AiOutlineHeart
+                  className="text-gray-600 bg-transparent h-[30px] w-[25px] "
+                  onClick={() => {
+                    setIsLiked(true);
+                  }}
+                />
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  });
 };
 
 export default HotelCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import HotelCard from "../components/HotelCard";
+import { CgSearch } from "react-icons/cg";
+import { BsFilterCircle } from "react-icons/bs";
 
 const Home = () => {
   return (
-    <div className=" ">
-      <div></div>
+    <div className="">
+      <div
+        className="p-2 my-2 min-w-full flex flex-row items-center justify-between text-black bg-slate-400 border-[1px]
+       border-black rounded-full  "
+      >
+        <div className="h-10 w-10">
+          <CgSearch className="min-h-full min-w-full" />
+        </div>
+        <div>Search for Any Location</div>
+        <div>
+          <BsFilterCircle />
+        </div>
+      </div>
+
       <div className=" grid gap-2 md:grid-cols-3 lg:grid-cols-4 ">
         <HotelCard />
         <HotelCard />
