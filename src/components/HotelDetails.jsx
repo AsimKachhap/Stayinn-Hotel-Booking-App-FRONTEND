88d8b68a4ca61d623ff8903e375829792c8cf347
unknown <asim26k@gmail.com>
2023-02-04 02:23:18 +0530

    Having issues dynamically displaying hotel details.

--- src/components/HotelDetails.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HotelDetails = () => {
-  return <div>HotelDetails</div>;
-};
-
-export default HotelDetails;
--- src/pages/Hotel.jsx
+++ src/pages/Hotel.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { AiFillStar } from "react-icons/ai";
 
 const Hotel = () => {
-  return (
-    <div>Hotel</div>
-  )
-}
+  const hotel = [
+    {
+      hotelName: "Shangri La",
+      city: "New Delhi",
+      state: "Delhi",
+      address: "19, Ashoka Rd, Janpath, Connauught Place, New Delhi, Delhi",
+      PIN: 11001,
+      startingPrice: 17500,
+      rating: 4,
+    },
+  ];
 
-export default Hotel
+  return (
+    <div className="">
+      <div className="flex-2 w-[vw] max-h-[250px] ">
+        <img
+          className=" w-full h-full"
+          src="/images/hotel1.jpg"
+          alt={hotel.hotelName}
+        />
+        <div className="grid grid-cols-2">
+          <div>
+            <h2>{hotel.hotelName}</h2>
+            <h3>
+              {hotel.city}, {hotel.state}
+            </h3>
+            <div>
+              <AiFillStar />
+              {hotel.rating}
+            </div>
+          </div>
+          <div>2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hotel;
